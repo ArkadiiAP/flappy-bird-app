241f1c03097634322b13a6bc3ec0ab9da35cbb0f
Arkadii <69986653+ArkadiiAP@users.noreply.github.com>
2021-01-04 10:33:49 +0200

    Update pipesReducer.js

--- src/store/reducers/pipesReducer.js
+++ src/store/reducers/pipesReducer.js
@@ -12,7 +12,7 @@ const pipesReducer = createSlice({
     reducers: {
         addNewPipe: (state) => {
             let height = Math.floor(Math.random() * 394) - 100;
-            if (height > 0) height = 1;
+            if (height < 0) height = 0;
             let { gameFieldWidth, gameFieldHeight } = state;
             state.arrayOfPipes.push({
                 pipeHeightTop: height,
